50fe452fa8bd3ecf903837ff62928a2b5e525795
Sergei Leonov <sleonov27@amherst.edu>
2026-04-21 11:50:48 -0400

    add freq sweep script

--- bosonic_bath.py
+++ bosonic_bath.py
@@ -12,6 +12,7 @@ Del = 0  # detuning term
 omega0 = 1.0  # GHZ frequency of the bath modes (normalized)
 Omega = 60*omega0  # GHz frequency of the driving field
 omega_drive = Omega/2.4048  # GHz frequency of the driving field (resonant)
+freq_list = np.linspace(omega_drive - 3, omega_drive + 3, 100) # driving freqs 
 
 # Bath properties:
 gamma = omega0/2 # GHz cut off frequency
@@ -20,7 +21,7 @@ T = 0.5  # K temperature
 
 # solver parameters:
 Nk = 2 # expansion terms
-time_steps = 10000
+time_steps = 1000
 solver_steps = 15000
 max_depth = 5  # maximum hierarchy depth to retain
 
@@ -30,9 +31,8 @@ psi0 = basis(2, 0) # ground state
 # time vector
 tmax = 30/omega0
 
-# ------------------Simulation------------------
-def simulate_tls_dynamics(psi0=psi0, time_drive = tmax, time_steps=time_steps, solver_steps=solver_steps, max_depth=max_depth, Q=sigmax()):
-
+# ------------------Simulation for 1 TLS------------------
+def simulate_tls_dynamics(psi0=psi0, time_drive = tmax, time_steps=time_steps, solver_steps=solver_steps, max_depth=max_depth, Q=sigmax(), omega_drive=omega_drive):
     # check that driving time is less than total simulation time
     if time_drive > tmax:
         raise ValueError("Driving time must be less than total simulation time.")
@@ -87,7 +87,7 @@ def simulate_tls_dynamics(psi0=psi0, time_drive = tmax, time_steps=time_steps, s
     
 
 
-# -----------------Plotting the results-----------------
+# -----------------Plotting the Bloch sphere-----------------
 def plot_bloch_sphere(result, tmax=tmax, filename="bloch_sphere.png"):
     
     xs, ys, zs, t = result
@@ -120,10 +120,63 @@ def plot_bloch_sphere(result, tmax=tmax, filename="bloch_sphere.png"):
     plt.savefig(filename)
 
 
-def main():
-    result = simulate_tls_dynamics(time_drive=15/omega0, Q=sigmax()+sigmaz())
-    plot_bloch_sphere(result)
+#----------------------Plotting heatmap----------------------
+def plot_freq_sweep_map(filename="freq_sweep.png",  freq_list=freq_list, psi0=psi0, time_drive = tmax, time_steps=time_steps, solver_steps=solver_steps, max_depth=max_depth, Q=sigmax()):
+    # check that driving time is less than total simulation time
+    if time_drive > tmax:
+        raise ValueError("Driving time must be less than total simulation time.")
+    
+    # initialize arrays for plotting
+    time = None
+    xs_all = np.zeros((len(freq_list), time_steps))
+    ys_all = np.zeros((len(freq_list), time_steps))
+    zs_all = np.zeros((len(freq_list), time_steps))
 
+    # iterate over drive frequencies
+    for i in range(len(freq_list)): 
+        xs, ys, zs, t = simulate_tls_dynamics(omega_drive=freq_list[i], psi0=psi0, time_drive=time_drive, time_steps=time_steps, 
+                                                        solver_steps=solver_steps, max_depth=max_depth, Q=Q)
+        if time is None: # initialize time array
+            time = t
+
+        # store data
+        xs_all[i] = xs
+        ys_all[i] = ys
+        zs_all[i] = zs
+
+    # transpose results
+    xs_all = np.transpose(xs_all)
+    ys_all = np.transpose(ys_all)
+    zs_all = np.transpose(zs_all)
+    
+    # plot results
+    xs_min, xs_max = -np.abs(xs_all).max(), np.abs(xs_all).max()
+    ys_min, ys_max = -np.abs(ys_all).max(), np.abs(ys_all).max()
+    zs_min, zs_max = -np.abs(zs_all).max(), np.abs(zs_all).max()
+   
+    fig, axes = plt.subplots(1, 3, figsize=(18, 5))
+
+    m0 = axes[0].pcolormesh(freq_list, time, xs_all, cmap ='inferno', shading='auto', vmin = xs_min, vmax = xs_max)
+    m1 = axes[1].pcolormesh(freq_list, time, ys_all, cmap ='inferno', shading='auto', vmin = ys_min, vmax = ys_max)
+    m2 = axes[2].pcolormesh(freq_list, time, zs_all, cmap ='inferno', shading='auto', vmin = zs_min, vmax = zs_max)
+    plt.colorbar(m0)
+    plt.colorbar(m1)
+    plt.colorbar(m2)
+
+    # add labels
+    axes[0].set_title("X expectation")
+    axes[1].set_title("Y expectation")
+    axes[2].set_title("Z expectation")
+    axes[0].set_ylabel('Time')
+    axes[1].set_xlabel('Frequency')
+
+    plt.savefig(filename) # save
+
+
+def main():
+    result = simulate_tls_dynamics(time_drive=10/omega0)
+    plot_bloch_sphere(result)
+    # plot_freq_sweep_map(time_drive=10/omega0)
 
 if __name__ == "__main__":
     main()
