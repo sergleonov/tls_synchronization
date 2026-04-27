8fd82fdc5b479718c6377eb51413bc0e798a6621
Sergei Leonov <sleonov27@amherst.edu>
2026-04-27 19:58:24 -0400

    add plotting of two husimi function for mark and non mark

--- husimi_animation_single_tls.py
+++ husimi_animation_single_tls.py
@@ -8,6 +8,8 @@ import matplotlib.pyplot as plt
 from qutip import *
 from tqdm import tqdm
 from scipy.special import jv
+from qutip.solver.heom import DrudeLorentzPadeBath
+from qutip.solver.heom import HEOMSolver
 
 # ---------------------- Parameters ----------------------
 omega_tls = 3.75 # tls frequency
@@ -18,13 +20,24 @@ k_max = 10 # maximum bessel index
 Omega_amp = 0.5 # drive amplitude
 omega_d = 3.0 # drive frequency
 
-T_drive = 50.0 # drive time
-T_total = 100.0 # total time
-dt = 0.5
-
+T_drive = 40.0 # drive time
+T_total = 80.0 # total time
+dt = 0.5 # time step
 tlist = np.arange(0, T_total, dt)
+time_steps = len(tlist)
 
-gamma = 0.002 # dissipation
+# solver parameters:
+Nk = 2 # expansion terms
+solver_steps = 10000
+max_depth = 5  # maximum hierarchy depth to retain
+
+# coupling operator
+coupling_op = sigmax()
+Temp = 0.5  # K temperature
+lam = 0.02  # coupling strength
+omega_cut = omega_tls/2 # GHz cut off frequency
+
+SAVE_ANIMATION = True
 
 # ---------------------- Disorder ----------------------
 np.random.seed(18)
@@ -39,13 +52,13 @@ sz = sigmaz()
 
 sm = sigmam()
 
-c_ops = [np.sqrt(gamma)*sm] # collapse op
+c_ops = [np.sqrt(lam)*sm] # collapse op
 
 # ---------------------- Pulse envelope ----------------------
 def pulse_env(t, args):
     return 1.0 if t <= T_drive else 0.0
 
-#-------------Hamiltonian--------------
+#-------------Markovian Hamiltonian--------------
 # eq. 14 from the paper
 def drive_z(t, args):
     z_drive = 0
@@ -59,15 +72,33 @@ def drive_y(t, args):
         y_drive += omega_tls * jv(2*k + 1, Omega_amp/omega_d) * np.sin((2*k + 1) * omega_d * t) * pulse_env(t, args)
     return y_drive
 
-H_static = 0.5 * omega_tls * jv(0, Omega_amp/omega_d) * sz + 0.5* detuning * sx
+H0_mark = 0.5 * omega_tls * jv(0, Omega_amp/omega_d) * sz + 0.5* detuning * sx
 
-H = [H_static, [sz, drive_z], [sy, drive_y]]
+H = [H0_mark, [sz, drive_z], [sy, drive_y]]
+
+#-------------Non-Markovian Hamiltonian--------------
+# time independent part of the Hamiltonian:
+H_sys = 0.5 * omega_tls * sigmaz() + 0.5 * detuning * sigmax() # static Hamiltonian
+# time dependent part of the Hamiltonian:
+def H_coeff(t, args):
+    return 0.5 * Omega_amp * np.cos(omega_d * t) * pulse_env(t, args)
+H_non_mark = [H_sys, [sigmax(), H_coeff]]
+
+# total Hamiltonian
+H_non_mark = QobjEvo(H_non_mark)
+
+# define bath and solver
+bath = DrudeLorentzPadeBath(coupling_op, lam=lam, gamma=omega_cut, T=Temp, Nk=Nk)
+options = {"nsteps": time_steps, "progress_bar": ''}
+heom_solver = HEOMSolver(H_non_mark, bath, max_depth=max_depth, options=options)
 
 #-------------Initial state--------------
-psi0 = H_static.groundstate()[1]
-
+psi0 = H0_mark.groundstate()[1]
+rho0 = psi0 * psi0.dag() # initial density matrix
 #-------------Solve--------------
-result = mesolve(
+
+# Markovian dynamics using Lindblad master equation
+result_mark = mesolve(
     H,
     psi0,
     tlist,
@@ -75,6 +106,9 @@ result = mesolve(
     [],
     options=Options(store_states=True)
 )
+
+# Non-Markovian dynamics using HEOM
+result_non_mark = heom_solver.run(rho0, tlist)
 
 # ---------------------- Spin coherent ----------------------
 def spin_coherent(theta, phi):
@@ -91,40 +125,63 @@ def husimi_Q(rho, theta_grid, phi_grid):
 
     return Q
 
-theta_grid = np.linspace(0, np.pi, 100)
-phi_grid = np.linspace(0, 2*np.pi, 100)
+theta_grid = np.linspace(0, np.pi, 80)
+phi_grid = np.linspace(0, 2*np.pi, 80)
 
 # ---------------------- Compute Q(t) ----------------------
-print("Computing Husimi-Q(t)...")
+print("Computing Markovian Husimi-Q(t)...")
 
-Q_time = np.zeros((len(tlist), len(theta_grid), len(phi_grid)))
+Qt_mark = np.zeros((len(tlist), len(theta_grid), len(phi_grid)))
 
-for t_idx, state in enumerate(tqdm(result.states)):
+for t_idx, state in enumerate(tqdm(result_mark.states)):
 
     rho = state
     if rho.isket:
         rho = ket2dm(rho)
 
     rho1 = ptrace(rho, 0)
-    Q_time[t_idx] = husimi_Q(rho1, theta_grid, phi_grid)
+    Qt_mark[t_idx] = husimi_Q(rho1, theta_grid, phi_grid)
+
+print("Computing Non-Markovian Husimi-Q(t)...")
+
+Qt_non_mark = np.zeros((len(tlist), len(theta_grid), len(phi_grid)))
+
+for t_idx, state in enumerate(tqdm(result_non_mark.states)):
+    
+    rho = state
+    if rho.isket:
+        rho = ket2dm(rho)
+
+    rho1 = ptrace(rho, 0)
+    Qt_non_mark[t_idx] = husimi_Q(rho1, theta_grid, phi_grid)
 
 # ---------------------- Animation ----------------------
-print("Starting animation...")
+print("Generating animation...")
 
-plt.figure(figsize=(7,6))
+fig, ax = plt.subplots(1, 2, figsize=(12,5))
 
-frames = np.arange(0, len(tlist), 1)
-
-vmax = np.max(Q_time)
+vmax = max(np.max(Qt_mark), np.max(Qt_non_mark))
 
 pause_time = 0.1
 
-for t_idx in frames:
+cbar = fig.colorbar(plt.cm.ScalarMappable(cmap='inferno', norm=plt.Normalize(vmin=0, vmax=vmax)), ax=ax, label="Q(θ,φ)")
 
-    plt.clf()
+for t_idx in range(time_steps):
 
-    plt.imshow(
-        Q_time[t_idx],
+    # clear frame
+    ax[0].clear()
+    ax[1].clear()
+
+    # add labels
+    ax[0].set_title("Markovian")
+    ax[1].set_title("Non-Markovian")
+    ax[0].set_xlabel("φ")
+    ax[1].set_xlabel("φ")
+    ax[0].set_ylabel("θ")
+
+    # add data
+    im0 = ax[0].imshow(
+        Qt_mark[t_idx],
         origin='lower',
         extent=[0, 2*np.pi, 0, np.pi],
         cmap='inferno',
@@ -132,10 +189,16 @@ for t_idx in frames:
         vmax=vmax
     )
 
-    plt.colorbar(label="Q(θ,φ)")
-    plt.xlabel("φ")
-    plt.ylabel("θ")
+    im1 = ax[1].imshow(
+        Qt_non_mark[t_idx],
+        origin='lower',
+        extent=[0, 2*np.pi, 0, np.pi],
+        cmap='inferno',
+        vmin=0,
+        vmax=vmax
+    )
 
+    # label time of the frame
     t_now = tlist[t_idx]
 
     if t_now <= T_drive:
@@ -145,8 +208,10 @@ for t_idx in frames:
         phase = "DRIVE OFF"
         color = 'red'
 
-    plt.title(f"t = {t_now:.1f} ns | {phase}", color=color)
+    fig.suptitle(f"Husimi Q(t) plot for Markovian and Non-Markovian time evolution\nt = {t_now:.1f} ns | {phase}", color=color)
 
+    # save if needed
+    if SAVE_ANIMATION:
+        plt.savefig(f"animation/husimi_frame_{t_idx:03d}.png")
     plt.pause(pause_time)
-
 plt.show()
