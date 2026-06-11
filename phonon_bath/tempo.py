process tensor TEMPO computation, collective exc still not fixed

phonon_bath/tempo.py
@@ -17,11 +17,11 @@ gamma_bath = 0.05
 T = 0.5 # temperature
 
 # time parameters
-T_total = 400 # ns
+T_total = 200 # ns
 T_drive = 100.0   # ns
-dt = 2.0 # ns
+dt = 5.0 # ns
 tcut = None
-epsrel = 1e-3
+epsrel = 1e-4
 
 SAVE_FIG = True # set to True to save figures
 
@@ -35,7 +35,7 @@ print(f"TLS frequencies: {omega_tls}")
 # time list and drive frequencies
 tlist = np.arange(0, T_total+dt, dt)
 n_time = len(tlist)
-omega_d_vals = np.linspace(3.0, 5.0, 100)
+omega_d_vals = np.linspace(3.0, 5.0, 200)
 
 ap = argparse.ArgumentParser(description="TEMPO Simulation for single TLS")
 ap.add_argument("--tag", type=str, default="", help="Tag for output files")
@@ -44,10 +44,66 @@ args = ap.parse_args()
 
 # operators
 sx = oqupy.operators.sigma("x")
+sz = oqupy.operators.sigma("z")
 sp = oqupy.operators.sigma("+")
 sm = oqupy.operators.sigma("-")
-sz = oqupy.operators.sigma("z")
-rho0 = oqupy.operators.spin_dm("z-")
+
+sx_list = []
+sz_list = []
+sp_list = []
+sm_list = []
+
+def tensor(mats: list):
+    res = mats[0]
+    for i in range(1, len(mats)):
+        res = np.kron(res, mats[i])
+    return res
+
+for i in range(N_TLS):
+    op_list = [np.eye(2) for _ in range(N_TLS)]
+    op_list[i] = sx
+    sx_list.append(tensor(op_list))
+
+    op_list[i] = sz
+    sz_list.append(tensor(op_list))
+
+    op_list[i] = sp
+    sp_list.append(tensor(op_list))
+
+    op_list[i] = sm
+    sm_list.append(tensor(op_list))
+    
+collective_sp  = sum(sp_list)
+collective_sm  = sum(sm_list)
+collective_exc = np.matmul(collective_sp, collective_sm)
+
+# initial state
+psi0 = np.array([[0] for _ in range(2*N_TLS)])
+psi0[-1] = [1] # ground state
+rho0 = np.matmul(psi0, np.transpose(psi0))
+
+# bath
+correlations = oqupy.PowerLawSD(alpha=lam,
+                                    zeta=2,
+                                    cutoff=gamma_bath,
+                                    cutoff_type="exponential",
+                                    temperature=T)
+bath = oqupy.Bath(sum(sx_list), correlations)
+
+# compute process tensor
+tempo_params = oqupy.TempoParameters(dt=dt, tcut=tcut, epsrel=epsrel)
+process_tensor = oqupy.pt_tempo_compute(bath=bath,
+                                        start_time=0.0,
+                                        end_time=T_total,
+                                        parameters=tempo_params)
+
+# hamiltonian
+H0 = sum(0.5 * omega_tls[i] * sz_list[i] for i in range(N_TLS))
+Hint = 0
+for i in range(N_TLS):
+    for j in range(i+1, N_TLS):
+        Hint += J * np.matmul(sz_list[i], sz_list[j])
+H_static = H0 + Hint
 
 def drive_coeff(t, args):
     if 0.0 <= t <= args['T_drive']:
@@ -61,29 +117,19 @@ def compute_tempo(omega_d):
             'Omega': Omega_amp,
             'T_drive': T_drive
         }
-    
     def ham(t):
-        return drive_coeff(t, args) * sx + 0.5 * omega_tls[0] * sz
+        return H_static + drive_coeff(t, args) * sum(sx_list)
     
     system = oqupy.TimeDependentSystem(ham)
-    correlations = oqupy.PowerLawSD(alpha=lam,
-                                    zeta=2,
-                                    cutoff=gamma_bath,
-                                    cutoff_type="exponential",
-                                    temperature=T)
-    bath = oqupy.Bath(sx, correlations)
-    tempo_params = oqupy.TempoParameters(dt=dt, tcut=tcut, epsrel=epsrel)
 
-    tempo_sys = oqupy.Tempo(system=system,
-                            bath=bath,
-                            initial_state=rho0,
-                            start_time=0.0,
-                            parameters=tempo_params)
-    dynamics = tempo_sys.compute(end_time=T_total, progress_type="silent")
+    dynamics = oqupy.compute_dynamics(process_tensor=process_tensor, 
+                                      system=system,
+                                      initial_state=rho0,
+                                      start_time=0.0,
+                                      progress_type="bar")
 
-    global sp, sm
-    t, exc_tempo = dynamics.expectations(np.matmul(sp, sm), real=True)
-    t, sp_tempo  = dynamics.expectations(sp, real=False)
+    t, exc_tempo = dynamics.expectations(np.matmul(collective_sp, collective_sm), real=True)
+    t, sp_tempo  = dynamics.expectations(collective_sp, real=True)
 
     return exc_tempo, sp_tempo
 
@@ -145,14 +191,12 @@ def run_sim():
     exc_list = np.zeros((len(omega_d_vals), n_time))
     sp_list  = np.zeros((len(omega_d_vals), n_time))
 
-    with ProcessPoolExecutor(max_workers=max(1, multiprocessing.cpu_count()-1)) as executor:
+    # with ProcessPoolExecutor(max_workers=max(1, multiprocessing.cpu_count()-1)) as executor:
 
-        for idx, (exc, sp_res) in enumerate(tqdm(executor.map(compute_tempo,omega_d_vals),
-                                         total=len(omega_d_vals),
-                                         desc="TEMPO Simulation")):
-            exc_list[idx,:], sp_list[idx,:] = exc, sp_res
+    for idx, omega_d in enumerate(omega_d_vals):
+        exc_list[idx,:], sp_list[idx,:] = compute_tempo(omega_d)
 
-        res = (exc_list, sp_list)
+    res = (exc_list, sp_list)
     
     plot_exc_map(res, omega_d_vals, tlist, args.tag)
     plot_sp_map(res, omega_d_vals, tlist, args.tag)